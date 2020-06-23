ab7fd2223c74493c990932da6e41c4eefffa9ee1
Tristan Guest <tristanguest@gmail.com>
2020-06-23 15:12:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,11 @@ const app = "I don't do much."
 
 var dodger = document.getElementById('dodger')
 
-var dodger = document.getElementById('dodger')
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+ 
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
