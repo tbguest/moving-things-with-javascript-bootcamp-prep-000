63c60b30da1c2a46b77fac96e15c7f0cb9ad5269
Tristan Guest <tristanguest@gmail.com>
2020-06-23 15:20:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ function moveDodgerLeft() {
 }
 
 function moveDodgerRight() {
-  var rightNumbers = dodger.style.right.replace('px', '')
+  var rightNumbers = dodger.style.left.replace('px', '')
   var right = parseInt(rightNumbers, 10)
  
-  if (right > 0) {
-    dodger.style.right = `${right + 1}px`
+  if (right < 360) {
+    dodger.style.left = `${right + 1}px`
   }
 }
 
